771ebd87a71f08c62efb4b485970d3ff47ba958b
anphuc99 <86525411+anphuc99@users.noreply.github.com>
2022-01-25 10:13:26 +0700

    ưerwer

---
+++ lua/script_common/database/FleaMarket.lua
@@ -0,0 +1,15 @@
+return {
+    {
+        idItem = "myplugin/f984e166-c74e-4bb4-b621-4eafb5372f30",
+	    idNPC = "myplugin/022c7741-ccd7-40ff-8dd2-79b4528f9f04",
+	    price = 100
+    },
+    {
+        idItem = "myplugin/f984e166-c74e-4bb4-b621-4eafb5372f30",
+	    idNPC = "myplugin/022c7741-ccd7-40ff-8dd2-79b4528f9f04",
+	    price = 200
+    },
+    option = {
+        primaryKey = {"idItem","idNPC"}
+    }
+}
---
+++ lua/script_common/database/NPC.lua
@@ -0,0 +1,9 @@
+return {
+    {
+        id = "022c7741-ccd7-40ff-8dd2-79b4528f9f04",
+        name = "abc"
+    },
+    option = {
+        primaryKey = "id"
+    }
+}
