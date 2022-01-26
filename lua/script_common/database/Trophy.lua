1aa32634c0221767ae036bda7916482498e26305
anphuc99 <86525411+anphuc99@users.noreply.github.com>
2022-01-26 16:44:30 +0700

    sdert

--- lua/script_common/database/Trophy.lua
+++ lua/script_common/database/Trophy.lua
@@ -1,12 +1,12 @@
 return {
-    -- {
-    --     id = "myplugin/bc498465-8619-4188-bc57-ff9a717a3787",
-    --     mineSpeed = 1*20
-    -- },
-    -- {
-    --     id = "myplugin/1aba122c-b1ad-491a-8ba3-0dfbd8cda0f6",
-    --     mineSpeed = 3*20
-    -- },
+    {
+        id = "myplugin/bc498465-8619-4188-bc57-ff9a717a3787",
+        mineSpeed = 1*20
+    },
+    {
+        id = "myplugin/1aba122c-b1ad-491a-8ba3-0dfbd8cda0f6",
+        mineSpeed = 3*20
+    },
     option = {
         primaryKey= "id",
         extents = "Equipment"
--- lua/script_common/database/recipe.lua
+++ lua/script_common/database/recipe.lua
@@ -1,17 +1,17 @@
 return {
-    -- {
-    --     id = 1,
-    --     Material = {
-    --         {
-    --             id = "myplugin/f984e166-c74e-4bb4-b621-4eafb5372f30",
-    --             num = 4
-    --         },
-    --         {
-    --             id = "myplugin/a55fe9e9-8a01-4a54-ab5b-af6fa6cc26b2",
-    --             num = 2
-    --         },
-    --     }
-    -- },
+    {
+        id = 1,
+        Material = {
+            {
+                id = "myplugin/f984e166-c74e-4bb4-b621-4eafb5372f30",
+                num = 4
+            },
+            {
+                id = "myplugin/a55fe9e9-8a01-4a54-ab5b-af6fa6cc26b2",
+                num = 2
+            },
+        }
+    },
     option = {
         primaryKey= "id",
     }
